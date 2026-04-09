906b0236639b351d6adaf121b85a59967698b2bd
DevDuhva <167563092+DevDuhva@users.noreply.github.com>
2026-04-09 13:38:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,29 @@ livros = [
     }
 ]
 
+@app.route('/')
+def home():
+    return {
+        "mensagem": "API Biblioteca funcionando",
+        "rotas": {
+            "GET /livros": "Listar livros",
+            "GET /livros/<id>": "Buscar livro por id",
+            "POST /livros": "Cadastrar livro",
+            "PUT /livros/<id>": "Atualizar livro",
+            "DELETE /livros/<id>": "Deletar livro"
+        }
+    }
+
 @app.route('/livros', methods=['GET'])
 def listar_livros():
     return jsonify(livros)
 
+@app.route('/livros/<int:id>', methods=['GET'])
+def buscar_livro(id):
+    for livro in livros:
+        if livro['id'] == id:
+            return jsonify(livro)
+    return {"erro": "Livro não encontrado"}, 404
 
 @app.route('/livros', methods=['POST'])
 def cadastrar_livro():
@@ -23,10 +42,8 @@ def cadastrar_livro():
     if not dados.get('titulo') or not dados.get('autor'):
         return {"erro": "Título e autor são obrigatórios"}, 400
 
-
     if 'ano' in dados and dados['ano'] < 0:
         return {"erro": "Ano inválido"}, 400
-
 
     for l in livros:
         if l['titulo'].lower() == dados['titulo'].lower():
@@ -46,15 +63,12 @@ def cadastrar_livro():
         "livro": novo_livro
     }, 201
 
-
-
 @app.route('/livros/<int:id>', methods=['PUT'])
 def atualizar_livro(id):
     for livro in livros:
         if livro['id'] == id:
             dados = request.json
 
-            # validação de ano
             if 'ano' in dados and dados['ano'] < 0:
                 return {"erro": "Ano inválido"}, 400
 
@@ -69,8 +83,6 @@ def atualizar_livro(id):
 
     return {"erro": "Livro não encontrado"}, 404
 
-
-
 @app.route('/livros/<int:id>', methods=['DELETE'])
 def deletar_livro(id):
     for livro in livros:
@@ -79,7 +91,6 @@ def deletar_livro(id):
             return {"mensagem": "Livro removido com sucesso"}
 
     return {"erro": "Livro não encontrado"}, 404
-
 
 if __name__ == '__main__':
     app.run(debug=True)
