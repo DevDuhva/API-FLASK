765df7dece8836f0ff0b157697028a372e8be7f4
Lab05_etec <Lab05_etec@etec>
2026-05-14 13:20:28 -0300

    mensagem do commit

---
+++ frontend/script.js
@@ -0,0 +1,44 @@
+const botao = document.getElementById("buscar");
+const areaUsuarios = document.getElementById("usuarios");
+
+botao.addEventListener("click", buscarUsuarios);
+
+function buscarUsuarios() {
+
+    fetch("https://api-flask-2-qjhw.onrender.com/usuarios")
+
+    .then(resposta => resposta.json())
+
+    .then(dados => {
+
+        areaUsuarios.innerHTML = "";
+
+        dados.forEach(usuario => {
+
+            areaUsuarios.innerHTML += `
+            
+                <div class="card">
+
+                    <h2>${usuario.nome}</h2>
+
+                    <p><strong>Email:</strong> ${usuario.email}</p>
+
+                    <p><strong>Telefone:</strong> ${usuario.telefone}</p>
+
+                </div>
+
+            `;
+        });
+
+    })
+
+    .catch(erro => {
+
+        areaUsuarios.innerHTML =
+        "<p>Erro ao carregar usuários.</p>";
+
+        console.log(erro);
+
+    });
+
+}
